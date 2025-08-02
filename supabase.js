d2c43d3e91318a5c8c70fcf526946fe2d53868c6
Travancore Royals <jibugibson@gmail.com>
2025-08-02 13:03:10 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 <script type="module">
   import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-  const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-  const SUPABASE_KEY = "YOUR_ANON_PUBLIC_KEY";
+  const SUPABASE_URL = "https://travancore-monitor.supabase.co";
+  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpeHdiaWJxYWR0dnhpb3ZkYXFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMTU5NDUsImV4cCI6MjA2OTY5MTk0NX0.N1kgsZZRQ8i0-zGTxq1UspDnA9GZzXSLAiX9oYbOf58";
 
   export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 </script>
